d9965f2bd2a402484ece1231b9683bf74430715e
fnumono <wangpramono@gmail.com>
2017-10-07 17:47:53 -0700

    adding category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171008000916) do
+ActiveRecord::Schema.define(version: 20171008003704) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string   "title"
@@ -18,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171008000916) do
     t.string   "company"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "category_id"
   end
 
 end
